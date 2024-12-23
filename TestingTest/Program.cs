a47ad3f36288797543331ffa5d993074c3662eea
“Kornel <kornelhajo@gmail.com>
2024-12-24 00:41:33 +0100

    Initial testing

---
+++ TestingTest/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class CalculatorTests
+{
+    [Test]
+    public async Task TestOne()
+    {
+        var result = AddTwo(1, 2);
+        
+        await Assert.That(result).IsEqualTo(3);
+        await Assert.That(result).IsNotEqualTo(2);
+    }
+    
+    private int AddTwo(int x, int y)
+    {
+        return x + y;
+    }
+}
